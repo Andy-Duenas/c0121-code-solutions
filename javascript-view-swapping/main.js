4e610c98655ed2904ef08f8978397829e8fcd1d4
Andy Duenas <Duenas.g.andy@gmail.com>
2021-02-05 09:46:21 -0800

    fixed issue

--- javascript-view-swapping/main.js
+++ javascript-view-swapping/main.js
@@ -4,14 +4,16 @@ var $tabList = document.querySelectorAll('.tab');
 var $viewList = document.querySelectorAll('.view');
 
 function handleClick(event) {
-  console.log('event.target.matches(".tab"):', event.target.matches('.tab'));
-  for (var i = 0; i < $tabList.length; i++) {
-    if (event.target === $tabList[i]) {
-      event.target.className = 'tab active';
-    } else {
-      $tabList[i].className = 'tab';
+  if (event.target.matches('.tab')) {
+    for (var i = 0; i < $tabList.length; i++) {
+      if (event.target === $tabList[i]) {
+        event.target.className = 'tab active';
+      } else {
+        $tabList[i].className = 'tab';
+      }
     }
   }
+
   if (event.target.matches('.tab')) {
     var dataView = event.target.getAttribute('data-view');
     for (var j = 0; j < $viewList.length; j++) {
