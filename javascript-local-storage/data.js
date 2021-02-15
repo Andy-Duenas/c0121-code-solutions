2cfc6ce36e9ebc83778d31c926646e7e4a951568
Andy Duenas <Duenas.g.andy@gmail.com>
2021-02-14 21:11:09 -0800

    Im not sure what I changed

--- javascript-local-storage/data.js
+++ javascript-local-storage/data.js
@@ -1,16 +1,30 @@
 /* exported todos */
 
 var todos = [];
-
+// There is an empty object literal assigned to the variable todos
 var previousTodosJSON = localStorage.getItem('javascript-local-storage');
-
+// getItem method of the localStorage object is being called with 1 arguement
+// the string java-script-storage, the return of the expression is being assigned
+// to the var  previousTodosJSON
 if (previousTodosJSON !== null) {
+// if statement with the condition of the var  previousTodosJSON is strictly not equal to null
+// with a opem curly brace for the code block
   todos = JSON.parse(previousTodosJSON);
+// parse method of the JSON object is being called with one arguement, the var preiousTodosJSON
+// and the return of the exp is being assigned to the var todos
 }
+// closing bracket for the if statement
 
 function handleUnload(event) {
+// function definition handleUnload with the parameter event
   var todosJSON = JSON.stringify(todos);
+  // stringify method of the object JSON is being called with one arguement
+  // the return is being assigned to the var todosJSON
   localStorage.setItem('javascript-local-storage', todosJSON);
+  // setItem method of the localStorage object is being called with two arguements
+  // the string javascript-local-storage and the var todosJSON
 }
-
+// closing curly brace for the function definition
 window.addEventListener('beforeunload', handleUnload);
+// assEventListener method of the window object is being called with two arguements
+// the string beforeunload and the var handleUnload;
--- javascript-local-storage/main.js
+++ javascript-local-storage/main.js
@@ -1,21 +1,35 @@
 /* global uuid, todos */
 
 var $todoForm = document.querySelector('#todo-form');
-
+// querySelector method of the document object is being called with 1 arguement
+// css selector id todo-form, return is being assigned to the var var $todoForm
 $todoForm.addEventListener('submit', function (event) {
+// addEventListener method of the $todoForm object is being called with two arguements,
+// the string submit and the anonymous function with one parameter event
+// followed by a open curly brace for the definition block
   event.preventDefault();
+  // preventDefault method of the event object is being called
   var todo = {
+  // an object literal assinged to the var todo
     todoId: uuid.v4(),
+    // todoId propety with the value of the return of the v4 method of the uuid object
     task: $todoForm.elements.task.value,
+    // task property with the value of the value property of the task objcet of the element object of the $todoForm object
     isCompleted: false
+    // isCompleted property has a value of false
   };
+  // closing curly brace for the object literal
   todos.push(todo);
+  // push method of the todos object is called with one arguement, the var todo
   $todoList.append(renderTodo(todo));
+  // append method of the $todosList object is called with one arguement, the return of the renderTodo with the arguement todo
   $todoForm.reset();
+  // reset method of the $todosList object is called
 });
+// closing curly brace for the anonymous function and closing paranthesis for the arguement list
 
 var $todoList = document.querySelector('#todo-list');
-
+// querySelector method of the document object is being called with 1 arguement the string todo-list
 $todoList.addEventListener('change', function (event) {
   var todoId = event.target.getAttribute('id');
   for (var i = 0; i < todos.length; i++) {
