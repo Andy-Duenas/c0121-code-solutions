7b93d766b78e877abecbe280de506768a88c2672
Andy Duenas <Duenas.g.andy@gmail.com>
2021-02-17 11:40:47 -0800

    completed ransomCase

--- string-manipulation-2/src/ransom-case.js
+++ string-manipulation-2/src/ransom-case.js
@@ -1,1 +1,15 @@
 /* exported ransomCase */
+
+function ransomCase(string) {
+  var lowerCaseString = string.toLowerCase();
+  var returnString = '';
+
+  for (var i = 0; i < string.length; i++) {
+    if (i % 2 === 1) {
+      returnString += lowerCaseString[i].toUpperCase();
+    } else {
+      returnString += lowerCaseString[i];
+    }
+  }
+  return returnString;
+}
