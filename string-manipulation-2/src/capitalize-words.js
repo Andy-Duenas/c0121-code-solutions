06844dd3c0c5766c9ff59eb9612ffe7efc0954ac
Andy Duenas <Duenas.g.andy@gmail.com>
2021-02-17 12:36:34 -0800

    working on capitalizeWords

--- string-manipulation-2/src/capitalize-words.js
+++ string-manipulation-2/src/capitalize-words.js
@@ -1,1 +1,16 @@
 /* exported capitalizeWords */
+function capitalizeWords(string) {
+  var lowerCaseString = string.toLowerCase();
+  var returnResult = '';
+  var isSpace = false;
+  returnResult += string[0].toUpperCase();
+  for (var i = 1; i < string.length; i++) {
+    if (string[i] === ' ') {
+      isSpace = true;
+    }
+    if (isSpace === false) {
+      returnResult += lowerCaseString[i];
+    }
+  }
+  return returnResult;
+}
