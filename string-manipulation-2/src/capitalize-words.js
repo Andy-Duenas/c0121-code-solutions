120e2f4d2f21d34f2df837ecceb984de30bd280c
Andy Duenas <Duenas.g.andy@gmail.com>
2021-02-20 00:02:30 -0800

    completed string-mainpulation-2

--- string-manipulation-2/src/capitalize-words.js
+++ string-manipulation-2/src/capitalize-words.js
@@ -2,13 +2,13 @@
 function capitalizeWords(string) {
   var lowerCaseString = string.toLowerCase();
   var returnResult = '';
-  var isSpace = false;
   returnResult += string[0].toUpperCase();
   for (var i = 1; i < string.length; i++) {
     if (string[i] === ' ') {
-      isSpace = true;
-    }
-    if (isSpace === false) {
+      returnResult += lowerCaseString[i].toUpperCase();
+      i++;
+      returnResult += lowerCaseString[i].toUpperCase();
+    } else {
       returnResult += lowerCaseString[i];
     }
   }
