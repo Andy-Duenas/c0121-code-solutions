bff2815dc2fbc554edba0b95dea06c888842114b
Andy Duenas <Duenas.g.andy@gmail.com>
2021-02-21 12:55:47 -0800

    Deleted unnecessary for loop for a return

--- string-manipulation-2/src/capitalize-word.js
+++ string-manipulation-2/src/capitalize-word.js
@@ -4,13 +4,7 @@ function capitalizeWord(word) {
   var result = '';
   word = word.toLowerCase();
   if (word === 'javascript') {
-    for (var i = 0; i < word.length; i++) {
-      if (word[i] === 'j' || word[i] === 's') {
-        result += word[i].toUpperCase();
-      } else {
-        result += word[i];
-      }
-    }
+    return 'JavaScript';
   } else {
     result = word[0].toUpperCase();
     result += word.slice(1);
