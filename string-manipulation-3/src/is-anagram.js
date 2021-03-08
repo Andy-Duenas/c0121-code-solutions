61e6945353e0a6ca47ee269681b494b1c86f42c4
Andy Duenas <Duenas.g.andy@gmail.com>
2021-03-08 12:31:08 -0800

    working on second problem.

--- string-manipulation-3/src/is-anagram.js
+++ string-manipulation-3/src/is-anagram.js
@@ -1,40 +1,26 @@
+/* eslint-disable eqeqeq */
 /* eslint-disable no-console */
 /* exported isAnagram */
 function isAnagram(firstString, secondString) {
   firstString = firstString.replace(/\s/g, '');
   secondString = secondString.replace(/\s/g, '');
+  var firstArray = [];
+  var secondArray = [];
+  firstArray.push(firstString.split('').sort());
+  secondArray.push(secondString.split('').sort());
   var found = 0;
-  var prevFound = [];
-  for (var i = 0; i < firstString.length; i++) {
-    for (var k = 0; k < secondString.length; k++) {
-      if (firstString[i] === secondString[k]) {
-        if (prevFoundHandler(prevFound, k, firstString, secondString)) {
-          break;
-        } else {
-          prevFound.push(k);
-          found += 1;
-          break;
-        }
-      }
-    }
-  }
-  if (found === secondString.length) {
-    return true;
-  } else {
-    return false;
-  }
-}
 
-function prevFoundHandler(prevArray, num, firstString, secondString) {
-  for (var a = 0; a < prevArray.length; a++) {
-    if (prevArray[a] === num) {
-      for (var i = num + 1; i < firstString.length; i++) {
-        if (secondString[num] === firstString[i]) {
-          console.log(secondString[num], firstString[i]);
-          return true;
-        }
+  for (var i = 0; i < firstArray.length; i++) {
+    for (var a = 0; a < secondArray.length; a++) {
+
+      if (firstArray[i] === secondArray[a]) {
+        found += 1;
+        break;
       }
     }
+  }
+  if (found === secondArray.length) {
+    return true;
   }
   return false;
 }
