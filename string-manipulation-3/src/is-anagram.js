f857fb7f871ff422fe4abc16603a1425bd9bd6b0
Andy Duenas <Duenas.g.andy@gmail.com>
2021-03-11 15:50:00 -0800

    completed string-manipulation-3

--- string-manipulation-3/src/is-anagram.js
+++ string-manipulation-3/src/is-anagram.js
@@ -1,26 +1,12 @@
-/* eslint-disable eqeqeq */
-/* eslint-disable no-console */
 /* exported isAnagram */
 function isAnagram(firstString, secondString) {
-  firstString = firstString.replace(/\s/g, '');
-  secondString = secondString.replace(/\s/g, '');
-  var firstArray = [];
-  var secondArray = [];
-  firstArray.push(firstString.split('').sort());
-  secondArray.push(secondString.split('').sort());
-  var found = 0;
+  firstString = firstString.replaceAll(' ', '');
+  secondString = secondString.replaceAll(' ', '');
 
-  for (var i = 0; i < firstArray.length; i++) {
-    for (var a = 0; a < secondArray.length; a++) {
-
-      if (firstArray[i] === secondArray[a]) {
-        found += 1;
-        break;
-      }
+  for (var i = 0; i < firstString.length; i++) {
+    if (!secondString.includes(firstString[i]) || !firstString.includes(secondString[i]) || firstString.length !== secondString.length) {
+      return false;
     }
   }
-  if (found === secondArray.length) {
-    return true;
-  }
-  return false;
+  return true;
 }
--- string-manipulation-3/src/title-case.js
+++ string-manipulation-3/src/title-case.js
@@ -1,1 +1,38 @@
+/* eslint-disable no-console */
 /* exported titleCase */
+function titleCase(title) {
+  title = title.toLowerCase().split(' ');
+  var results = '';
+  for (var i = 0; i < title.length; i++) {
+    if (title[i].length <= 3) {
+      if (title[i] === 'api') {
+        results += 'API' + ' ';
+      } else if (title[i] === title[0] || title[i] === 'web') {
+        results += title[i][0].toUpperCase() + title[i].slice(1) + ' ';
+      } else if (title[i - 1].includes(':')) {
+        results += title[i][0].toUpperCase() + title[i].slice(1) + ' ';
+      } else {
+        results += title[i] + ' ';
+      }
+    } else {
+      if (title[i].includes('javascript')) {
+        if (title[i].includes(':')) {
+          results += 'JavaScript:' + ' ';
+        } else {
+          results += 'JavaScript' + ' ';
+        }
+      } else if (title[i].includes('-')) {
+        var replace = title[i][title[i].indexOf('-') + 1];
+        var capital = title[i][title[i].indexOf('-') + 1].toUpperCase();
+        var word = title[i];
+
+        word = word.replace(replace, capital);
+        word = word[0].toUpperCase() + word.slice(1) + ' ';
+        results += word;
+      } else {
+        results += title[i][0].toUpperCase() + title[i].slice(1) + ' ';
+      }
+    }
+  }
+  return results.trim();
+}
