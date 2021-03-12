e949b4492acb5af3b3e0cec44ca780d0b280d354
Andy Duenas <Duenas.g.andy@gmail.com>
2021-03-11 16:50:03 -0800

    Made adjustments to code and added a guard.

--- string-manipulation-3/src/is-anagram.js
+++ string-manipulation-3/src/is-anagram.js
@@ -2,9 +2,11 @@
 function isAnagram(firstString, secondString) {
   firstString = firstString.replaceAll(' ', '');
   secondString = secondString.replaceAll(' ', '');
-
+  if (firstString.length !== secondString.length) {
+    return false;
+  }
   for (var i = 0; i < firstString.length; i++) {
-    if (!secondString.includes(firstString[i]) || !firstString.includes(secondString[i]) || firstString.length !== secondString.length) {
+    if (!secondString.includes(firstString[i]) || !firstString.includes(secondString[i])) {
       return false;
     }
   }
