0069e48ee6b0cd51ba67ff7745b732b8417af422
Andy Duenas <Duenas.g.andy@gmail.com>
2021-03-11 18:02:47 -0800

    completed ex 2

--- array-manipulation-3/src/zip.js
+++ array-manipulation-3/src/zip.js
@@ -1,1 +1,19 @@
 /* exported zip */
+
+function zip(first, second) {
+  const newArray = [];
+
+  let length = 0;
+  if (first.length < second.length) {
+    length = first.length;
+  } else {
+    length = second.length;
+  }
+  for (let i = 0; i < length; i++) {
+    const smallArray = [];
+    smallArray.push(first[i]);
+    smallArray.push(second[i]);
+    newArray.push(smallArray);
+  }
+  return newArray;
+}
