8654005aae7b70474c7346592114790b4696693d
Andy Duenas <Duenas.g.andy@gmail.com>
2021-03-16 15:50:52 -0700

    made array on demand and client receives 201

--- express-post-json/index.js
+++ express-post-json/index.js
@@ -2,8 +2,7 @@ const express = require('express');
 const app = express();
 
 let nextIndex = 1;
-
-const gradesArray = [];
+const grades = {};
 
 app.listen(3000, () => {
   // eslint-disable-next-line no-console
@@ -11,7 +10,12 @@ app.listen(3000, () => {
 });
 
 app.get('/api/grades', (req, res) => {
-  res.status(200).json(gradesArray);
+  const jsonArray = [];
+  for (const key in grades) {
+    const obj = grades[key];
+    jsonArray.push(obj);
+  }
+  res.status(200).json(jsonArray);
 });
 
 app.use(express.json(), (req, res, next) => {
@@ -19,12 +23,12 @@ app.use(express.json(), (req, res, next) => {
 });
 
 app.post('/api/grades', (req, res) => {
-  const grades = {};
-  grades.id = nextIndex;
+  const newGrade = {};
+  newGrade.id = nextIndex;
   for (const key in req.body) {
-    grades[key] = req.body[key];
+    newGrade[key] = req.body[key];
   }
-  gradesArray.push(grades);
+  grades[nextIndex] = newGrade;
   nextIndex++;
-  res.status(200).send('Created');
+  res.status(201).send(newGrade);
 });
