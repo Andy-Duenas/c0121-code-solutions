efe1a8a34d66e3db4012d53d441a7fb6a52970ef
Andy Duenas <Duenas.g.andy@gmail.com>
2021-03-18 11:36:41 -0700

    completed sql-join

---
+++ sql-join/big-spenders.sql
@@ -0,0 +1,7 @@
+select "payments".*,
+        "customers"."firstName",
+        "customers"."lastName"
+  from "payments"
+  join "customers" using ("customerId")
+  order by "amount" desc
+  limit 10;
