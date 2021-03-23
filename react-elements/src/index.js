c5f91db37a4bdeba65d17c580603597ee48d6a9b
Andy Duenas <Duenas.g.andy@gmail.com>
2021-03-23 16:16:24 -0700

    completed react-elements

---
+++ react-elements/src/index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const reactElement = React.createElement('div', null, 'Hello React!');
+ReactDOM.render(reactElement, document.getElementById('root'));
