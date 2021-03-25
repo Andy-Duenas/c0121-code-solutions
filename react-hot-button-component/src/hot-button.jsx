49d9a33f027a98068a65386444cc6e1447c5b30d
Andy Duenas <Duenas.g.andy@gmail.com>
2021-03-25 14:32:14 -0700

    fixed issues

--- react-hot-button-component/src/hot-button.jsx
+++ react-hot-button-component/src/hot-button.jsx
@@ -3,31 +3,28 @@ import React from 'react';
 class hotButton extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { isClicked: false };
+    this.state = { count: 0 };
     this.handleClick = this.handleClick.bind(this);
-    this.i = 0;
   }
 
   handleClick() {
-    if (this.state.isClicked === false) {
-      return this.setState({ isClicked: true });
-    }
-    return this.setState({ isClicked: false });
+    return this.setState({ count: this.state.count + 1 });
   }
 
-  handleColor(i) {
-    if (i >= 3 && i < 6) { return 'blue'; }
-    if (i >= 6 && i < 9) { return 'purple'; }
-    if (i >= 9 && i < 12) { return 'red'; }
-    if (i >= 12 && i < 15) { return 'orange'; }
-    if (i >= 15 && i < 18) { return 'yellow'; }
-    if (i >= 18) { return 'white'; }
+  handleColor() {
+    if (this.state.count >= 3 && this.state.count < 6) { return 'blue'; }
+    if (this.state.count >= 6 && this.state.count < 9) { return 'purple'; }
+    if (this.state.count >= 9 && this.state.count < 12) { return 'red'; }
+    if (this.state.count >= 12 && this.state.count < 15) { return 'orange'; }
+    if (this.state.count >= 15 && this.state.count < 18) { return 'yellow'; }
+    if (this.state.count >= 18) { return 'white'; }
   }
 
   render() {
+    const color = this.handleColor();
     return (
-      <button onClick={(this.handleClick)}
-       className={(this.handleColor(this.i++))}>Hot Button</button>
+      <button onClick={this.handleClick}
+        className={color}>Hot Button</button>
     );
   }
 }
