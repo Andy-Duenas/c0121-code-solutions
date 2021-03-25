3ebe9427c4b62b213b7cfdb62d99e0814ec21fdf
Andy Duenas <Duenas.g.andy@gmail.com>
2021-03-24 20:06:38 -0700

    completed react-events-and-state

---
+++ react-events-and-state/src/index.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class CustomButton extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { isClicked: false };
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    if (this.setState({ isClicked: true }));
+  }
+
+  render() {
+    return (
+      <button onClick={(this.handleClick)}>
+        { !this.state.isClicked ? 'click me!' : 'Yay!'}
+      </button>
+    );
+  }
+}
+
+ReactDOM.render(
+  <CustomButton />,
+  document.getElementById('root')
+);
